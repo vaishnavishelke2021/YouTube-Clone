fcef78cf8665f041bc7a579f4267f01beb809f34
Vaishnavi Shelke <89862255+vaishnavishelke2021@users.noreply.github.com>
2024-05-15 00:45:52 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,3 @@
-const GOOGLE_API_KEY = "AIzaSyAB3EsWHsfbYIyPnXyXcbygrN13NLjFM2A";
+const GOOGLE_API_KEY = "";
 
-export const YOUTUBE_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
-  GOOGLE_API_KEY;
+export const YOUTUBE_API = "" + GOOGLE_API_KEY ;
